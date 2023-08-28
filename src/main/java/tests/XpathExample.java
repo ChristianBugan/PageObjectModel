e16711f4f22cd2317da8425e2be14d0e45ea1393
unknown <christian.bugan@gmail.com>
2023-08-28 11:23:53 +0300

    curs24

--- src/main/java/tests/XpathExample.java
+++ src/main/java/tests/XpathExample.java
@@ -1,10 +1,15 @@
 package tests;
 
+import java.time.Duration;
+import java.util.List;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import utils.BaseTest;
 
@@ -51,8 +56,11 @@ public class XpathExample extends BaseTest {
 	@Test(priority=2)
 	public void xpathExample2() throws InterruptedException {
 		
-		Thread.sleep(3000);
+		//Thread.sleep(5000);
 		//driver.findElement(By.xpath("//span[@class='user_name']")).click();
+		
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[@class='user_name']")));
 	
 		WebElement user = driver.findElement(By.xpath("//span[@class='user_name']"));
 		Actions action =  new Actions(driver);
@@ -72,15 +80,54 @@ public class XpathExample extends BaseTest {
 		//Xpath
 		//      --> //a[contains(@href, 'profile.php' )]
 		
-		Thread.sleep(2000);
+		Thread.sleep(2500);
 		driver.findElement(By.xpath("//a[contains(@href, 'profile.php' )]")).click();
+		
+		Thread.sleep(1000);
 		
 		//WebElement  : <a href="https://keybooks.ro/account/orders/">recent orders</a>
 		
 		
-		
 		// --> //a[contains(text(), 'orde')]
 		driver.findElement(By.xpath("//a[contains(text(), 'orde')]")).click();
+		
+		//	//th[contains(@class, 'woocommerce-orders-table__header' )]/span[contains(text(), 'Order')]	
+		
+		JavascriptExecutor jse = (JavascriptExecutor) driver;
+		
+		WebElement orderTableHeader = driver.findElement(
+				By.xpath("//th[contains(@class, 'woocommerce-orders-table__header' )]/span[contains(text(), 'Order')]"));
+		
+		jse.executeScript("arguments[0].setAttribute('style', 'background: orange;border:4px solid blue')", orderTableHeader);
+		
+		
+		//index based
+		// --> (//th[contains(@class, 'woocommerce-orders-table__header')]/span)[4]
+		
+		WebElement totalTableHeader = driver.findElement(
+				By.xpath("(//th[contains(@class, 'woocommerce-orders-table__header')]/span)[4]"));
+				
+		jse.executeScript("arguments[0].setAttribute('style', 'background: orange;border:4px solid blue')", totalTableHeader);
+		
+		// //td[contains(@class, 'woocommerce-orders-table__cell' )][@data-title='Order']/a[contains(text(), '1720')]
+		
+		WebElement order1720 = driver.findElement(
+				By.xpath("//td[contains(@class, 'woocommerce-orders-table__cell' )][@data-title='Order']/a[contains(text(), '1720')]"));
+		
+		jse.executeScript("arguments[0].setAttribute('style', 'background: orange;border:4px solid blue')", order1720);
+		
+		// FUNCTIA NOT 
+		
+		// //td[contains(@class, 'woocommerce-orders-table__cell' )][@data-title='Order']/a[not(contains(text(), '1720'))]
+		
+		List<WebElement> orders = driver.findElements(
+				By.xpath("//td[contains(@class, 'woocommerce-orders-table__cell' )][@data-title='Order']/a[not(contains(text(), '1720'))]"));
+		
+		for(WebElement element : orders) {
+			jse.executeScript("arguments[0].setAttribute('style', 'background: orange;border:4px solid red')", element);
+		}
+		
+			
 		
 	}
 	
